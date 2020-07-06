bd297606ce3129508fc599e27553fed8d4add389
rgonz522 <rgonz522@fiu.edu>
2020-07-06 12:01:29 -0700

    Initial commit

---
+++ app/src/main/java/com/example/parstagram/LogInActivity.java
@@ -0,0 +1,77 @@
+package com.example.parstagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LogInActivity extends AppCompatActivity {
+
+    private static final String TAG = "LogInActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btLogIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        if(ParseUser.getCurrentUser() != null ){ goMainActivity();}
+
+        etUsername = findViewById(R.id.etUserName);
+        etPassword = findViewById(R.id.etPassword);
+        btLogIn    = findViewById(R.id.btnLogIn);
+
+        btLogIn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username,password);
+            }
+        });
+
+
+
+
+    }
+
+    private void loginUser(String username, String password)
+    {
+        Log.i(TAG, "loginUser: Attempting to Login" + username );
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e)
+            {
+                if(e != null)
+                {
+                    //TODO : better error handling and ui
+                    Log.e(TAG, "issue with login: ",e );
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(LogInActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainActivity()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -0,0 +1,23 @@
+package com.example.parstagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("rebeca-parstagram") // should correspond to APP_ID env variable
+                .clientKey("CodePathMoveFastParsestagram")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server("https://rebeca-parstagram.herokuapp.com/parse/").build());
+
+    }
+}
